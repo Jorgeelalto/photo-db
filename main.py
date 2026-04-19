f5e2211a046a417c7de322a8de501eae8469669b
Jorge Marcos Chávez <jormarchav@gmail.com>
2026-04-19 11:29:43 +0200

    Reworked DB into a class, scan, dump into file

--- main.py
+++ main.py
@@ -1,7 +1,81 @@
+from pathlib import Path
+import json
 import yaml
+import os
+import re
 
-config = {}
-with open("config.yml") as config_file:
-	config = yaml.safe_load(config_file)
 
-print(config)
+PATTERN_YEAR = re.compile(
+	'^(19[0-9]{2})|(20[0-9]{2})$'
+	)
+PATTERN_PHOTO = re.compile(
+	'^IMG_[0-9]{8}_[0-9]{6}\\.(heic|jpg|jpeg|png|mov|mp4)$',
+	flags=re.IGNORECASE)
+IGNORE = [
+		".DS_Store"
+	]
+
+
+class PhotoDB:
+
+	config = {}
+	db = {}
+
+	def __init__(self, config_path: str):
+		try:
+			with open(config_path) as config_file:
+				self.config = yaml.safe_load(config_file)
+		except Exception as e:
+			print(f"Couldn't load config file: {str(e)}")
+		self.db['photos'] = {}
+
+
+	def photo_get(self, id: str):
+		if self.db['photos'][id]:
+			return self.db['photos'][id].copy()
+		else:
+			return None
+
+	def photo_set(self, id: str, path: str):
+		photo = {}
+		photo['path'] = path
+		self.db['photos'][id] = photo
+		print(f"added {id} ({photo['path']})")
+		return photo
+
+
+	def scan(self):
+		root = Path(self.config['photo_folder'])
+		years = os.listdir(root)
+		l = []
+		for y in years:
+			if PATTERN_YEAR.match(y):
+				l.append(y)
+		years = l
+		years.sort()
+
+		for y in years:
+			for t in os.listdir(root / y):
+				if t not in IGNORE:
+					files = os.listdir(root / y / t)
+					for f in files:
+						if f not in IGNORE:
+							if PATTERN_PHOTO.match(f):
+								self.photo_set(os.path.splitext(f)[0], f"{y}/{t}/{f}")
+
+
+	def save(self):
+		try:
+			with open(self.config['database_file'], mode='w') as db_file:
+				return json.dump(self.db, db_file, sort_keys=True)
+		except Exception as e:
+			print(f"Couldn't write database to file: {str(e)}")
+
+
+	def find_bad_naming(self):
+		return False
+
+
+p = PhotoDB('config.yml')
+p.scan()
+p.save()
